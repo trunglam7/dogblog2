0e81dc35f4199c0edde772e1dc796951016db41b
Trung Lam <trunglam107@gmail.com>
2023-05-25 08:23:39 -0700

    testing github action

--- app/backend/contentful.js
+++ app/backend/contentful.js
@@ -3,7 +3,7 @@ import {createClient} from 'contentful';
 const client = createClient({
     space: 'cjy6wcf5u1iq',
     environment: 'master', // defaults to 'master' if not set
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+    accessToken: process.env.CONTENTFUL_API_KEY
 })
 
 export default client;
